ac1faa4d9a5d2a4f4a3a7bb0b964c1362f249606
Henrique Marmo <henrique.marmo15@gmail.com>
2023-01-17 08:20:34 -0300

    Sububindo Atividade 2

--- SAPCommerce.java
+++ SAPCommerce.java
@@ -5,5 +5,7 @@ public class SAPCommerce{
     public void removeItemFromCart(){
         System.out.println("Removendo item do carrinho");
     }
-    
+    public void updateItemFromCart(){
+        System.out.println("Atualiza item do carrinho");
+    }
 }
--- TestaSAPCommerce.java
+++
@@ -1,8 +0,0 @@
-public class TestaSAPCommerce{
-    public static void main (String [] args){
-        SAPCommerce sapcommerce = new SAPCommerce();
-
-        sapcommerce.addToCart();
-        sapcommerce.removeItemFromCart();
-    }
-}
